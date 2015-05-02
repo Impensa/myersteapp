aac05e9bc17ffc4c064435e9642d7a28afdb913f
Impensa <jkingston@kingstoncarnegie.com>
2015-05-02 22:41:30 +0200

    My first model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,31 @@
 class PostsController < ApplicationController
+	
 	def index
+		@content_first = " This is some sample text omfg"
+		@contents_second = " This is some sample text omfgggggg"
+	end
+
+	def new
+
+	end
+
+	def create
+
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def show
+
+	end
+
+	def destroy
 
 	end
 end
