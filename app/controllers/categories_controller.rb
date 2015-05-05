256db514609f77cc2daf8087eb7295849a33ef67
Impensa <jkingston@kingstoncarnegie.com>
2015-05-05 21:40:23 +0200

    Keep on truckin

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,16 @@
 class CategoriesController < ApplicationController
+
   def index
-  end
-
-  def edit
-  end
-
-  def new
+    @categories = Category.all
   end
 
   def show
+    @category = Category.find(params[:id])
+    @title = @category.name
+    @posts = @category.posts
+    #can do this because we made the association in the models
+    #why was line 10 wrong when singular?
+    #why was line 9 wrong when caps?
   end
+
 end
